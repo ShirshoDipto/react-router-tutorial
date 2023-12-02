c6124d991563da492ab899845d821d888b868f17
ShirshoDipto <shirshodipto@gmail.com>
2023-12-02 08:58:55 +0600

    Apply basic crud with react router dom

---
+++ src/main.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Contact, { loader as contactLoader } from "./routes/Contact";
+import EditContact, { action as editAction } from "./routes/Edit";
+import Destroy, { action as destroyAction } from "./routes/Destroy";
+import Index from "./routes/Index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        element: <Destroy />,
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  // <RouterProvider router={router} />
+);
